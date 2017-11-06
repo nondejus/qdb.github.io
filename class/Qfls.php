4777855fd772dfb9e5ff1b925bf2220aea66f70e
Piazzi Raffaele <quanticodb@gmail.com>
2017-11-07 00:34:22 +0100

    Code Optimization

---
+++ class/Qfls.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Quantico;
+
+class Qfls extends Qout
+{
+    protected static function flusso($numkey, $keyval, $tot, $n, $opz=0, $numkeyass=0)
+    {
+        $ok = true; $si = false;
+        if(!isset($numkey[13])) $numkey[13] = null; 
+        if(!isset($numkey[14])) $numkey[14] = null; 
+        if(!isset($numkey[17])) $numkey[17] = null;
+         
+        if(is_array($numkeyass)) { 
+            if($numkeyass[14] || $numkeyass[17]) { 
+                if(!$opz) { $ok = false; $numkey[18][] = $numkeyass[1]; 
+                    for($a=0; $a<18; $a++) $numkey[$a] = $numkeyass[$a]; 
+                    for($a=0; $a<$n; $a++) $keyval[$numkey[1]][$a] = $keyval[$a]; 
+                    if(isset($keyval['t.0'])) { for($a=0; $a<$n; $a++) $keyval['t.'.$numkey[1]][$a] = $keyval[$a]; }
+                } else $numkey = $numkeyass; 
+            } else { 
+                if($numkey[9]) { 
+                    if(!$numkey[18] || is_array($numkey[18])) $ok = false; 
+                    if($numkey[17]) $ok = true; 
+                }
+            }
+        } 
+        
+        if($numkey[14] == '' && $numkey[16] == '') { 
+            if(is_array($numkey[18])) { $t = $numkey[18]; 
+                foreach($t as $x) { $y = Qout::analisi($x); 
+                    if($y[14]) { $ok = false; if($y[18]) { $numkey[18] = true; $ok = true; } else $numkey[18] = false; $numkey[14] = $y[14]; $numkey[1] = $y[1]; } 
+                    if($y[17]) { $numkey[1] = $y[16]; $numkey[16] = $y[16]; $numkey[17] = $y[17]; $ok = true; $si = true; break; }
+                }
+            }
+        }
+         
+        if($numkey[14]) {
+            if($opz == 1) { 
+                if($numkey[1][0] == '@') { 
+                    for($a=0; $a<$n; $a++) $keyval[$numkey[1]][$a] = $keyval['p.'.$a]; 
+                } else { 
+                    for($a=0; $a<$n; $a++) $keyval[$numkey[1]][$a] = $keyval[$a]; 
+                }
+            } 
+            elseif($opz == 2) { 
+                if($numkey[1][0] == '#' || $numkey[1][0] == '@') { 
+                    for($a=0; $a<$n; $a++) $keyval[$numkey[1]][$a] = $keyval[$a]; 
+                }
+            } else { 
+                if($numkey[1][0] == '#' || $numkey[1][0] == '@') { 
+                    for($a=0; $a<$n; $a++) { 
+                        if(isset($keyval['N.'.$numkey[1].'.'.$a])) { 
+                            for($b=0; $b<$keyval['N.'.$numkey[1].'.'.$a]; $b++) $keyval[$numkey[1]][] = $keyval[$numkey[1].'.'.$a][$b]; 
+                        }
+                    }
+                }
+            } $kk['K'][0] = $numkey[1];
+            if(!isset($keyval[$numkey[1]])) return false;
+            
+            if(isset($numkey[20])) { $x = []; 
+                foreach($keyval[$numkey[1]] as $a) { $a = SYS::trak($a,true); if(is_array($a)) $x = array_merge($x,$a); else $x[] = $a; } 
+                $keyval[$numkey[1]] = $x; 
+            } 
+            for($a=0, $u=count($keyval[$numkey[1]]); $a<$u; $a++) if($keyval[$numkey[1]][$a] === false || $keyval[$numkey[1]][$a] === null) $keyval[$numkey[1]][$a] = ''; 
+            $k = array_count_values($keyval[$numkey[1]]); $av = array_keys($k); $an = array_values($k); $n = 0; 
+            for($a=0, $u=count($av); $a<$u; $a++) { $kk[$numkey[1]][$a] = $av[$a]; $kk['N.'.$numkey[1]][$a] = $an[$a]; $n += $an[$a]; }
+
+            if($numkey[17]) { 
+                if($numkey[18] && $ok) { 
+                    if($numkey[17] == 1) array_multisort($kk['N.'.$numkey[1]],SORT_DESC,SORT_NUMERIC,$kk[$numkey[1]]); 
+                    elseif($numkey[17] == 2) array_multisort($kk['N.'.$numkey[1]],SORT_ASC,SORT_NUMERIC,$kk[$numkey[1]]); 
+                } else { 
+                    if($numkey[17] == 1) array_multisort($kk[$numkey[1]],SORT_ASC,SORT_FLAG_CASE,$kk['N.'.$numkey[1]]); 
+                    elseif($numkey[17] == 2) array_multisort($kk[$numkey[1]],SORT_DESC,SORT_FLAG_CASE,$kk['N.'.$numkey[1]]); 
+                }
+            } 
+            $kk['T.'.$numkey[1]] = $n; 
+            $kk['N'] = count($av); 
+            $kk['T'] = $tot; 
+            if($kk['T'] > 0) return $kk;
+        }
+        else
+        { 
+            if($numkey[17] == '' && is_array($numkey[13])) { 
+                foreach($numkey[13] as $a) { $b = $a[strlen($a)-1]; 
+                    if($b == '+') { $numkey[17] = 1; $numkey[16] = substr($a,0,-1); $j = array_search($a,$numkey[18]); 
+                        if($j !== false) $numkey[18][$j] = $numkey[16]; break; 
+                    } 
+                    elseif($b == '-') { $numkey[17] = 2; $numkey[16] = substr($a,0,-1); $j = array_search($a,$numkey[18]); 
+                        if($j !== false) $numkey[18][$j] = $numkey[16]; break; 
+                    }
+                } 
+                if(!isset($keyval[$numkey[16]])) return Qerror(2, 30, $a); 
+            } 
+            if($numkey[17]) { $t = false; $x = -1; 
+                if($opz) { 
+                    for($a=0; $a<$n; $a++) $keyval[$numkey[1]][$a] = $keyval[$a]; 
+                    if(isset($keyval['-.0'])) $numkey[18] = array('-','p'); else $numkey[18] = []; 
+                } else { 
+                    if(is_array($numkey[18])) { $b = $numkey[16]; 
+                        if($si) { if($numkey[17] == 1) $b .= '+'; else $b .= '-'; } $j = array_search($b,$numkey[18]); 
+                        if($j !== false) unset($numkey[18][$j]); $numkey[18] = SYS::sort($numkey[18]); 
+                    }
+                } 
+                if(isset($keyval['t.0']) || $numkey[0] == '@2') { $t = true; if($opz) $numkey[18][] = 't'; } for($a=0; $a<$n; $a++) $keyval['Q'][$a] = $a;
+                if(isset($keyval[$numkey[16]])) $si = $numkey[16]; else $si = $numkey[1];
+                
+                if($t && !$opz) { 
+                    if($numkey[17] == 1) array_multisort($keyval[$si],SORT_ASC,SORT_FLAG_CASE,$keyval['Q'],$keyval["t.$si"]); 
+                    elseif($numkey[17] == 2) array_multisort($keyval[$si],SORT_DESC,SORT_FLAG_CASE,$keyval['Q'],$keyval["t.$si"]); 
+                } else {
+                    if($numkey[17] == 1) array_multisort($keyval[$si],SORT_ASC,SORT_FLAG_CASE,$keyval['Q']); 
+                    elseif($numkey[17] == 2) array_multisort($keyval[$si],SORT_DESC,SORT_FLAG_CASE,$keyval['Q']); 
+                }
+                 
+                if($numkey[0] != '@1' && $numkey[0] != '@2') { 
+                    foreach($keyval['Q'] as $a) { $x++; $kk[$x] = $keyval[$a]; if($t && !$opz) $kk["t.$x"] = $keyval["t.$a"]; }
+                } 
+                if($opz) { 
+                    foreach($numkey[18] as $k) { $x = -1; foreach($keyval['Q'] as $a) { $x++; $kk["$k.$x"] = $keyval["$k.$a"]; }}
+                } else { if(isset($keyval['K'])) $kk['K'] = SYS::u($keyval['K']); 
+                    if($ok) { $kk[$si] = $keyval[$si]; if($t) $kk["t.$si"] = $keyval["t.$si"]; $kk["N.$si"] = $keyval["N.$si"]; }
+                    if(isset($kk['K'])) { 
+                        foreach($kk['K'] as $k) { $x = -1; 
+                            foreach($keyval['Q'] as $a) { $x++; 
+                                if($k[0] == '#' || $k[0] == '@') { 
+                                    if(isset($keyval["$k.$a"])) { 
+                                        $kk["$k.$x"] = $keyval["$k.$a"]; 
+                                        if($t) $kk["$k.t.$x"] = $keyval["$k.t.$a"]; 
+                                        $kk["N.$k.$x"] = $keyval["N.$k.$a"]; 
+                                        $kk["T.$k.$x"] = $keyval["T.$k.$a"]; 
+                                    }
+                                } else { 
+                                    if($si != $k) { $kk[$k][$x] = $keyval[$k][$a]; if($t) $kk["t.$k"][$x] = $keyval["t.$k"][$a]; }
+                                }
+                            } if(isset($keyval["N.$k"])) $kk["N.$k"] = $keyval["N.$k"]; else $kk["N.$k"] = 0; 
+                        }
+                    }
+                } 
+                $kk['N'] = $n; 
+                $kk['T'] = $tot; 
+                if($kk['T'] > 0) return $kk; 
+            } 
+            $keyval['N'] = $n; // $keyval['P'] = SYS::$partial; 
+            $keyval['T'] = $tot; 
+            if($keyval['T'] > 0) return $keyval; 
+        }
+        return false;
+    }
+}
---
+++ class/Qmxf.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Quantico;
+
+class Qmxf extends Qmix
+{
+    protected static function mixflusso($keyval, $key, $keyk, $keyl, $numkey, $numkeyass, $dati, $ccc, $opz, $valass, $valok)
+    {
+        $numkeyass[1] = SYS::sort($numkeyass[1]); $w = 0; $ky = [];
+         
+        if(is_array($numkeyass[1])) { $ok = true; 
+            foreach($numkeyass[1] as $key) { $dati = Qout::analisi($key); 
+                if($dati[1][0] == '#' || $dati[1][0] == '@') $k = explode(' ',substr($dati[1],1)); 
+                else { 
+                    if($dati[1][0] == '_') $k = explode(' ',substr($dati[1],1)); else $k = explode(' ',$dati[1]); 
+                    $ok = true; 
+                } 
+                foreach($k as $x) { $y = explode('.',$x); 
+                    foreach($y as $z) if(!in_array($z."\n",QKEYBASE)) { $ok = false; break; }
+                } 
+                if($ok) { 
+                    $keyval['K'][$w] = $dati[1]; 
+                    $ky[$w] = $key; $w++; 
+                } else Qerror(0,1,$dati[1]); 
+            } 
+            $ky = array_reverse($ky); $x = $w; 
+        } else { 
+            $keyval['K'][0] = $numkeyass[1]; 
+            $ky[0] = $key; $x = 1; 
+        } 
+        if(!isset($keyval[0])) return false; 
+        
+        $dat = []; $si = false; 
+        foreach($ky as $key) { $x--; $y = $keyval['K'][$x]; 
+            if($key[0] == '#' || $key[0] == '@') { 
+                for($a=0; $a<$keyk; $a++) { 
+                    $val = Qout::Qdbout($numkey[1].' '.$key,$keyval[$a],$opz); 
+                    if($val) { 
+                        if($opz === _T1_ || $opz === _T2_) { 
+                            for($b=0; $b<$val['N']; $b++) { 
+                                $keyval["$y.t.$a"][$b] = $val["t.$b"]; 
+                                unset($val["t.$b"]); 
+                            }
+                        } 
+                        $keyval["$y.$a"] = $val; 
+                        unset($keyval["$y.$a"]['N']); 
+                        unset($keyval["$y.$a"]['T']); 
+                        $keyval["N.$y.$a"] = $val['N']; 
+                        $keyval["T.$y.$a"] = $val['T']; 
+                        if(isset($dat["N.$y"])) $dat["N.$y"]++; else $dat["N.$y"] = 1; 
+                    }
+                }
+            } else { $si = true; 
+                if($valass == null || $valass === _T1_ || $valass === _T2_) { 
+                    if(strpos($valok,'#') > 1) { 
+                        for($a=0; $a<$keyk; $a++) { $w = 0; 
+                            if($opz) { 
+                                for($b=0; $b<$keyval["N.#$numkey[1].$a"]; $b++) { 
+                                    $val = Qout::Qdbout($numkey[1].' '.$key,$keyval["#$numkey[1].$a"][$b],$opz); 
+                                    $keyval["$y.$a"][$b] = $val[$key]; 
+                                    $keyval["$y.t.$a"][$b] = $val["t.$key"]; 
+                                    if($keyval["$y.$a"][$b] != '') $w++; 
+                                } 
+                                $keyval["N.$y.$a"] = $w; 
+                            } else { 
+                                for($b=0; $b<$keyval["N.#$numkey[1].$a"]; $b++) { 
+                                    $keyval["$y.$a"][$b] = Qout::Qdbout($numkey[1].' '.$key,$keyval["#$numkey[1].$a"][$b]); 
+                                    if($keyval["$y.$a"][$b] != '') $w++; 
+                                } 
+                                $keyval["N.$y.$a"] = $w; 
+                            }
+                        } 
+                        $keyval["N.$y"] = $keyval["N.#$numkey[1]"]; 
+                    } else { $w = 0; 
+                        for($a=0; $a<$keyk; $a++) { 
+                            if($opz) { 
+                                if($ccc) $val = Qout::Qdbout("@$numkey[1] $key",$keyval[$a],$opz); else $val = Qout::Qdbout("$numkey[1] $key",$keyval[$a],$opz); 
+                                $keyval[$y][$a] = $val[$y]; 
+                                $keyval["t.$y"][$a] = $val["t.$y"]; 
+                            } else { 
+                                if($ccc) $keyval[$y][$a] = Qout::Qdbout("@$numkey[1] $key",$keyval[$a]); else $keyval[$y][$a] = Qout::Qdbout("$numkey[1] $key",$keyval[$a]); 
+                            } 
+                            if($keyval[$y][$a] != '') $w++; 
+                        } 
+                        $keyval["N.$y"] = $w; 
+                    }
+                } else { $w = 0; 
+                    for($a=0; $a<$keyk; $a++) { 
+                        if($opz) { 
+                            $val = Qout::Qdbout("$numkey[1] $key",$keyval[$a],$opz); 
+                            $keyval[$y][$a] = $val[$y]; 
+                            $keyval["t.$y"][$a] = $val["t.$y"]; 
+                        } else $keyval[$y][$a] = Qout::Qdbout("$numkey[1] $key",$keyval[$a]); 
+                        if($keyval[$y][$a] != '') $w++; 
+                    } 
+                    $keyval["N.$y"] = $w; 
+                }
+            }
+        } if($si) { 
+            for($a=0; $a<$keyk; $a++) { 
+                if($opz) unset($keyval["#$numkey[1].t.$a"]); 
+                unset($keyval["#$numkey[1].$a"]); 
+                unset($keyval["N.#$numkey[1].$a"]); 
+                unset($keyval["T.#$numkey[1].$a"]); 
+            } 
+            unset($keyval["N.#$numkey[1]"]); 
+        } 
+        if($dat) $keyval = array_merge($keyval,$dat); 
+        $keyval['N'] = $keyk; 
+        $keyval['T'] = $keyl; 
+        if($numkeyass[14]) $numkeyass[1] = $numkeyass[14]; else $numkeyass[18] = $ky; 
+        if($keyval['N'] > 0) {
+            if($numkey[14]) $numkey[14] = false;
+            require_once 'Qfls.php'; $Qdb = new SYS; 
+            return $Qdb->_flusso($numkeyass,$keyval,$keyval['T'],$keyval['N'],0,$numkey); 
+        } 
+        return false;
+    }
+}
