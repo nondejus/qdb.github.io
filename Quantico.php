1e2c7796d2c9f95c99ae3f93024ed51251a0089f
Piazzi Raffaele <quanticodb@gmail.com>
2017-12-16 20:21:50 +0100

    Protected from unauthorized calls

--- Quantico.php
+++ Quantico.php
@@ -4,10 +4,12 @@ namespace Quantico; if($Qmaintenance) return array('maintenance' => $Qmaintenanc
 
 class DB
 {
-    public static function in($k=null, $v=null, $s=null, $o=0) { if(is_array($k)) return false; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qin.php'; $v = Qin::Qdbin($k,$v,$s,$o); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
-    public static function ver($k=null, $v=null, $s=null, $o=null) { if(is_array($k)) return false; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qver.php'; $v = Qver::Qdbver($k,$v,$s,$o); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
-    public static function del($k=null, $v=null, $s=null) { if(is_array($k)) return false; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qdel.php'; if(strpos($k,'#') > 1 || strpos($k,',') > 2) $v = Qdel::Qdbdel($k,$v,$s); else $v = Qdel::Qdbdel($k,$v,$s,0); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
-    public static function out($k=null, $v=null, $s=null, $o=null) { if(is_array($k)) return false; require_once 'class/Qout.php'; return Qout::Qdbout($k,$v,$s,$o); }
+    private static function exit($k, $v, $s, $o){ if(!$k || is_array($k) || function_exists($k)) return true; if(!is_array($v) && function_exists($v)) return true; if(!is_array($s) && function_exists($s)) return true; if(!is_array($o) && function_exists($o)) return true; return false; }
+    
+    public static function in($k=null, $v=null, $s=null, $o=0){ if(DB::exit($k,$v,$s,$o)) return null; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qin.php'; $v = Qin::Qdbin($k,$v,$s,$o); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
+    public static function ver($k=null, $v=null, $s=null, $o=null){ if(DB::exit($k,$v,$s,$o)) return null; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qver.php'; $v = Qver::Qdbver($k,$v,$s,$o); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
+    public static function del($k=null, $v=null, $s=null){ if(DB::exit($k,$v,$s,0)) return null; global $Qdatabase; $x = fopen("$Qdatabase/lock.php","w"); if(flock($x,LOCK_EX) == false) { fclose($x); return null; } else { require_once 'class/Qdel.php'; if(strpos($k,'#') > 1 || strpos($k,',') > 2) $v = Qdel::Qdbdel($k,$v,$s); else $v = Qdel::Qdbdel($k,$v,$s,0); Qsync('*'); flock($x,LOCK_UN); fclose($x); return $v; }}
+    public static function out($k=null, $v=null, $s=null, $o=null){ if(DB::exit($k,$v,$s,$o)) return null; require_once 'class/Qout.php'; return Qout::Qdbout($k,$v,$s,$o); }
 }
 
 ?>
