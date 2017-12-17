d274a1235a5973853282a679044da99e41054e6f
Piazzi Raffaele <quanticodb@gmail.com>
2017-12-17 15:10:49 +0100

    Protected from unauthorized calls

--- Qadmin/database.php
+++ Qadmin/database.php
@@ -140,7 +140,7 @@
             });}
         }
         function carica(x) { vl = $('#cmd' + x).val(); com = vl.split('::'); com = com[1].substring(0,3); vl = vl.replace(/'/g,'#####1'); vl = vl.replace(/"/g,'#####2'); $('#gio').html('<img src="images/load.gif" style="float:right; margin-bottom:6px">');
-            $.post("keybase.php", { type: 11, val: vl, old: 1, pos: x }, function(msg){ if(msg == 'OK') { $('#rst').load('temp/risultato.php').show(); if(com == 'in(' || com == 'del' || com == 'ver') $('#gio').html('<a class="agg" href="javascript:aggiorna()"><?=$lng[13]?></a>'); else if(com == 'out') $('#gio').html('&emsp;<a class="agg" href="javascript:tabella()"><?=$lng[12]?></a>'); else if(com == 'tim' || com == 'dir') $('#gio').html(''); }});
+            $.post("keybase.php", { type: 11, val: vl, old: 1, pos: x }, function(msg){ if(msg == 'OK') { $('#rst').load('temp/risultato.php').show(); if(com == 'in(' || com == 'del' || com == 'ver') $('#gio').html('<a class="agg" href="javascript:aggiorna()"><?=$lng[13]?></a>'); else if(com == 'out') $('#gio').html('&emsp;<a class="agg" href="javascript:tabella()"><?=$lng[12]?></a>'); else if(com == 'tim' || com == 'dir') $('#gio').html(''); } else $('#gio').html(''); });
         }
         function cerca(x) { if(x) { sito = 'query.php?id='+dbcl; titolo = 'QUERY'; } else { sito = 'cerca.php?id='+dbcl; titolo = '<?=$lng[48]?>'; }
             $lb.launch({ url: sito, options: "afterEnd:aggiorna width:820 navTop:true titleTop:true", title: "<h3 align='center'>" + titolo + "</h3>" })
